0f16d084e03087b53aa5e75cf24b0251579f23dc
ne-elnur <elnurkazy.urmat89@gmail.com>
2022-12-28 00:24:59 +0600

    base for back

--- .idea/index.js
+++ .idea/index.js
@@ -1,4 +1,35 @@
-let a = 5;
-let b = 10;
+import express from 'express';
+import jwt from 'jsonwebtoken';
 
-console.log(a + b, 'Hello');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello cutie');
+});
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+
+    const token = jwt.sign(
+        {
+            email: req.body.email,
+            fullName: 'Elnurkazy Urmat',
+        },
+        'secret123',
+    );
+
+    res.json({
+        success: true,
+        token,
+    });
+});
+
+app.listen(2002, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Server OK');
+});
