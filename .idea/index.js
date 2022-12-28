7b5214f81cfd7894f48f20edf4263425d096d686
ne-elnur <elnurkazy.urmat89@gmail.com>
2022-12-28 12:25:51 +0600

    base for back 2

--- .idea/index.js
+++ .idea/index.js
@@ -1,5 +1,17 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
+
+import UserModel from './modules/User.js';
+
+mongoose
+    .connect('mongodb+srv://admin:1234@cluster0.udd5bpj.mongodb.net/ballsports?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err));
 
 const app = express();
 
@@ -9,21 +21,39 @@ app.get('/', (req, res) => {
     res.send('Hello cutie');
 });
 
-app.post('/auth/login', (req, res) => {
-    console.log(req.body);
+// app.post('/auth/login', (req, res) => {
+//     console.log(req.body);
+//
+//     if (req.body.email = 'el@nur.com') {
+//         const token = jwt.sign(
+//             {
+//                 email: req.body.email,
+//                 fullName: 'Elnurkazy Urmat',
+//             },
+//             'secret123',
+//         );
+//     }
 
-    const token = jwt.sign(
-        {
-            email: req.body.email,
-            fullName: 'Elnurkazy Urmat',
-        },
-        'secret123',
-    );
+app.post('/auth/register', registerValidator, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
 
-    res.json({
-        success: true,
-        token,
+    const password = req.body.password;
+    const solt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash,
     });
+
+    const user = await doc.save();
+
+    res.json(user);
 });
 
 app.listen(2002, (err) => {
