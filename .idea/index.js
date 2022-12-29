f043837b9c735858a0f6dacea8211c84a12c4385
ne-elnur <elnurkazy.urmat89@gmail.com>
2022-12-29 10:42:48 +0600

    base for back (login added)

--- .idea/index.js
+++ .idea/index.js
@@ -7,9 +7,10 @@ import { validationResult } from 'express-validator';
 import { registerValidation } from './validations/auth.js';
 
 import UserModel from './modules/User.js';
+import checkAuth from './utils/checkAuth.js';
 
 mongoose
-    .connect('mongodb+srv://admin:1234@cluster0.udd5bpj.mongodb.net/ballsports?retryWrites=true&w=majority')
+    .connect('mongodb+srv://admin:1234@cluster0.yriui7h.mongodb.net/ballsports?retryWrites=true&w=majority')
     .then(() => console.log('DB ok'))
     .catch((err) => console.log('DB error', err));
 
@@ -21,39 +22,112 @@ app.get('/', (req, res) => {
     res.send('Hello cutie');
 });
 
-// app.post('/auth/login', (req, res) => {
-//     console.log(req.body);
-//
-//     if (req.body.email = 'el@nur.com') {
-//         const token = jwt.sign(
-//             {
-//                 email: req.body.email,
-//                 fullName: 'Elnurkazy Urmat',
-//             },
-//             'secret123',
-//         );
-//     }
 
-app.post('/auth/register', registerValidator, async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
+app.post('/auth/login', async (req, res) => {
+   try{
+       const user = await UserModel.findOne({ email: req.body.email });
+
+       if (!user) {
+           return res.status(404).json({
+               message: "User not found"
+           });
+       }
+
+       const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+       if (!isValidPass) {
+            return res.status(400).json({
+                message: "false login or password"
+            });
+       }
+
+       const token = jwt.sign({
+               _id: user._id,
+           },
+           'secret123',
+           {
+               expiresIn: '30d'
+           },
+       );
+
+       const { passwordHash, ...userData } = user._doc;
+
+       res.json({
+           ...userData,
+           token,
+       });
+   } catch (err) {
+       console.log(err);
+       res.status(500).json({
+           message: "Authorization failed",
+       })
+   }
+});
+
+
+
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        });
+
+        const user = await doc.save();
+
+        const token = jwt.sign({
+            _id: user._id,
+        },
+            'secret123',
+            {
+                expiresIn: '30d'
+            },
+            );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Не удалось зарегистрироваться",
+        })
     }
+});
 
-    const password = req.body.password;
-    const solt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+app.get('/auth/me', checkAuth, async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId);
 
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
-    });
+        if(!user) {
+            return res.status(404).json({
+                message: 'User not found'
+            });
+        }
 
-    const user = await doc.save();
+        const { passwordHash, ...userData } = user._doc;
 
-    res.json(user);
+        res.json(userData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "No me access",
+        })
+    }
 });
 
 app.listen(2002, (err) => {
